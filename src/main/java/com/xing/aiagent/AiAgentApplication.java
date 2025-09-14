e9f36b456faff01653bf4c76d5cfaf98b1633c77
ZhaoXing <lalaeric@qq.com>
2025-09-14 18:03:15 +0800

    AI大模型接入+AI应用开发
    1. 新建项目，完成后端初始化
    2. 整合 Hutool 和 Knife4j 依赖
    3. 整合 Spring AI、LangChain4j 等框架，实现 4 种程序调用 AI 大模型的示例代码
    
    1. 实现多轮对话 AI 恋爱大师应用
    2. 自定义日志 Advisor、ReReading Advisor
    3. 实现 AI 恋爱报告生成功能（结构化输出）
    4. 自定义基于文件的会话记忆

---
+++ src/main/java/com/xing/aiagent/AiAgentApplication.java
@@ -0,0 +1,14 @@
+package com.xing.aiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("nacos.logging.default.config.enabled","false");
+        SpringApplication.run(AiAgentApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xing/aiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,6 @@
+package com.xing.aiagent.demo.invoke;
+
+public interface TestApiKey {
+
+    String API_KEY = "sk-4ae26e88ab7d421d978bf621a336f127";
+}
