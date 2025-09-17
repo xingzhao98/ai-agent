79a652a107568fbce6f13bcd677cf70770f71e69
ZhaoXing <lalaeric@qq.com>
2025-09-17 11:58:50 +0800

    AI 服务化 1. 后端增加 AI 接口（SSE 和同步 2 种方式） 2. 生成前端项目

--- src/main/java/com/xing/aiagent/app/LoveApp.java
+++ src/main/java/com/xing/aiagent/app/LoveApp.java
@@ -18,6 +18,7 @@ import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -146,6 +147,16 @@ public class LoveApp {
         String content = response.getResult().getOutput().getText();
         log.info("content: {}", content);
         return content;
+    }
+
+    public Flux<String> doChatByStream(String message, String chatId) {
+        return chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .stream()
+                .content();
     }
 
 }
